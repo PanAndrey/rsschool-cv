b8ef55e67d92bb7d4e57bab3b8c34f754343f1aa
PanAndrey <panadr09@gmail.com>
2021-09-19 21:58:05 +0400

    feat: add menu

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	// плавное перемещение страницы к нужному блоку
+	$("nav li a").click(function () {
+		elementClick = $(this).attr("href");
+		destination = $(elementClick).offset().top;
+		$("body,html").animate({scrollTop: destination }, 800);
+	});
+});
+
+console.log(1)
